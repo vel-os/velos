91d3b9ef34253532a2645f6f3fd6a453134522fa
kuberlog <collinalexbell@gmail.com>
2020-11-16 04:32:50 -0600

    boot.asm prints Velos using tty interupt

--- boot.asm
+++ boot.asm
@@ -1,5 +1,21 @@
-loop:
-	jmp loop
+mov ah, 0x0e ;tty mode
+
+mov al, 'V'
+int 0x10
+
+mov al, 'e'
+int 0x10
+
+mov al, 'l'
+int 0x10
+
+mov al, 'o'
+int 0x10
+
+mov al, 's'
+int 0x10
+
+jmp $
 
 times 510-($-$$) db 0
 dw 0xaa55
